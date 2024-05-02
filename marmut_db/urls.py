58c56760e9138166b77080cadbaed8ff043617db
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-05-02 21:14:50 +0700

    added bagian kuning FE and added bootstrap

--- marmut_db/urls.py
+++ marmut_db/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('register/user/', views.register_user, name='register_user'),
     path('register/label/', views.register_label, name='register_label'),
+    path('langganan_paket/', views.langganan_paket_page, name='langganan_paket'),
+    path('langganan_paket/submit/', views.langganan_paket_submit, name='langganan_paket_submit'),
+    path('langganan_paket/purchase_history', views.purchase_history, name='purchase_history'),
+    path('downloaded_songs/', views.downloaded_songs, name='downloaded_songs'),
+    path('search/', views.search, name='search'),
 ]
